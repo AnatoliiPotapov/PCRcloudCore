295915f90f64e1f11ecf9104fff0db157e34daad
Anatoly <anatolii.s.potapov@gmail.com>
2015-12-04 10:56:08 +0300

    Рефакторинг

--- R/Experiment.R
+++ R/Experiment.R
@@ -7,33 +7,27 @@ Experiment <- R6Class("Experiment",
                           self$name <- name
                           self$id <- id
                           private$parsed_file <- read_dt(file_path)
-                        },
-
-                        set_curves = function() {
                           private$raw_curves <- get_curves(private$parsed_file)
                         },
 
-                        preprocess = function() {
-
-                        },
-
-                        plot = function(mode = "raw", indexes = NULL, name = "PCR curves") {
-                          if (is.null(indexes)) { indexes = c(1:48)}
-                          if (mode == "raw") { plot_curves(asub(private$raw_curves,indexes), name) }
-                          if (mode == "cpp") { plot_curves(asub(private$cpp_curves,indexes), name) }
-                        },
-
-                        cpp = function() {
+                        preprocess = function( parameters = DEFAULT_PREPROCESSING ) {
                           pre_cpp <- lapply(private$raw_curves, function(curve) {
-
                             amptester(curve)
                           })
                           private$cpp_curves <- lapply(pre_cpp, function(curve) {
-
-                            #filter_outliners(
-                              preprocess_curve(curve)
-                            #)
+                            preprocess_curve(curve)
                           })
+                        },
+
+                        plot = function(mode = "RAW", indexes = NULL, name = "PCR curves") {
+                          if (is.null(indexes)) { indexes = c(1:48)}
+
+                          if (mode == "RAW") {
+                            plot_curves(asub(private$raw_curves, indexes))
+                          } else
+                          if (mode == "CPP") {
+                            plot_curves(asub(private$cpp_curves, indexes))
+                          }
                         }
 
                       ),
